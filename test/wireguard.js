a37035a90e5dd64b60ec58f2e31d9c3d16b8a247
Pedro Dias <petermdias@gmail.com>
2023-03-20 19:58:46 +0000

    lets go

--- test/wireguard.js
+++ test/wireguard.js
@@ -1,25 +1,22 @@
+const { expect } = require('chai');
 var config = require('../config');
 
 var Wireguard = require('../lib/wireguard');
-var Peer = require('../lib/peer');
 
 describe('Wireguard', function () {
-  var wireguard = new Wireguard(config.ips, config.private_key, config.listen_port, config.interfaces);
-
-  for (var i = 0; i < config.peers.length; i++) {
-    wireguard.peers.push(new Peer(config.peers[i].name, config.peers[i].ips, config.peers[i].public_key, config.peers[i].acl));
-  }
+  var wireguard = new Wireguard();
 
   describe('#generateConf()', function () {
-    it('should generate a valid wireguard configuration', function (done) {
-      var conf = wireguard.generateConf();
-
-      console.log(conf);
+    it('should load directly from conf file', function (done) {
+      wireguard.loadConfig(config);
       done();
     });
 
-    it('should load directly from conf file', function (done) {
-      wireguard.loadConfig(config);
+    it('should generate a valid wireguard configuration', function (done) {
+      var conf = wireguard.generateConf();
+
+      expect(conf).ok;
+
       done();
     });
   });
